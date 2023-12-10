a47e45d1d561dade7f040f4bc2c04a270d50f16d
MattuW4 <>
2023-12-10 17:38:15 +0000

    Create PostList in views.py. Create style.css file in static folder. Create base and post_list files in templates folder

--- csapp/views.py
+++ csapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from csapp.models import Post, Comment, Category
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    """
+    View for displying all blog posts on blog page, filtering by approved 
+    and ordered by descending date to display 6 blog posts per page.
+    """
+    model = Post
+    queryset = Post.objects.all(status=1).order_by('-created_on')
+    template_name = "index.html"
+    paginate_by = 6
