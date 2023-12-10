209f038ac0d8ac9b93337a37936014add8bb99a6
MattuW4 <>
2023-12-10 15:37:31 +0000

    Install summernote and update urls.py

--- csapp/admin.py
+++ csapp/admin.py
@@ -1,19 +1,23 @@
 from django.contrib import admin
 from csapp.models import Post, Comment, Category
+from django_summernote.admin import SummernoteModelAdmin
 
 
-class PostAdmin(admin.ModelAdmin):
-    pass
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """Admin post model"""
+    summernote_fields = ('content')
 
 
 class CommentAdmin(admin.ModelAdmin):
+    """Admin comment model"""
     pass
 
 
 class CategoryAdmin(admin.ModelAdmin):
+    """Admin category model"""
     pass
 
 
-admin.site.register(Post, PostAdmin)
 admin.site.register(Comment, CommentAdmin)
 admin.site.register(Category, CategoryAdmin)
