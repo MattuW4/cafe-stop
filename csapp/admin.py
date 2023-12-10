92123abdd2fad40dda2f4d70986d6790c3fcb000
MattuW4 <>
2023-12-10 14:12:35 +0000

    Create superuser. Add models to admin.py

--- csapp/admin.py
+++ csapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from csapp.models import Post, Comment, Category
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Category, CategoryAdmin)
