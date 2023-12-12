38226259e19d97dfb1c4df406083d37812d44a8d
MattuW4 <>
2023-12-12 09:50:16 +0000

    Create Post Detail view, template and URL. Update browse.html post link with Post Detail view

--- csapp/urls.py
+++ csapp/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.Index.as_view(), name='home'),
     path('browse', views.PostList.as_view(), name='browse'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     ]
