c9d95dea614ead731bb74f7a170b0d0fdc270b7c
MattuW4 <>
2023-12-28 21:49:29 +0000

    Correct broken url link for post cards in browse and category pages. Add authentication check on user if not logged in and attempting to comment on or like a post to PostDetail view

--- csapp/views.py
+++ csapp/views.py
@@ -74,6 +74,10 @@ class PostDetail(View):
     """
 
     def post(self, request, slug, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return redirect(
+                reverse('login.html')
+            )
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('-created_on')
