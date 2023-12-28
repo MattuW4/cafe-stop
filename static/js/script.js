95dad3139667481ba3999567e070f8428f27b87c
MattuW4 <>
2023-12-28 10:40:19 +0000

    Reduce message notification display by 0.5seconds.

--- static/js/script.js
+++ static/js/script.js
@@ -5,5 +5,5 @@ function closeAlerts () {
         let messages = document.getElementById("msg");
         let alert = new bootstrap.Alert(messages);
         alert.close();
-    }, 3000);
+    }, 2500);
 };
