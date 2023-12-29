4c2e0bfbc2d0b886e64892f1f6d64a893a9504b6
MattuW4 <>
2023-12-29 20:39:33 +0000

    Add and update tests in tests_views for CRUD and site navigation functionality that includes user authentication

--- csapp/tests_views.py
+++ csapp/tests_views.py
@@ -7,14 +7,13 @@ from .models import Post, Category, Comment
 class TestViews(TestCase, Client):
 
     def setUp(self):
-        """Set up before each text"""
-        self.user = User.objects.create_user(username='testuser', password='password')
+        """Set up before each test"""
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='password')        
         self.client.login(username='testuser', password='password')
         self.category = Category.objects.create(name='Test Category')
-        self.client = Client()
-        self.browse_url = reverse('browse')
         
-        post = Post.objects.create(
+        self.post = Post.objects.create(
             title='Test title',
             slug='test-title',
             author=self.user,
@@ -28,23 +27,90 @@ class TestViews(TestCase, Client):
             category=self.category
         )
 
-    # def test_model_Post(self):
-    #     
+        """Urls for test reverses"""
+        self.browse_url = reverse('browse')
+        self.add_post_url = reverse('add')
+        self.update_post_url = reverse('update', args=[self.post.slug])
+        self.delete_post_url = reverse('delete', args=[self.post.id])
         
-    #     self.assertEquals(str(post), 'Test title')
-    #     self.assertTrue(isinstance(post, Post))
+    def test_home_GET(self):
+        """Test to check home page is accessible when not logged in"""
+        self.client.logout()
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
 
     def test_home_GET(self):
+        """Test to check home page is accessible"""
         response = self.client.get('/')
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html', 'base.html')
 
     def test_browse_GET(self):
+        """Test to check browse page is accessible"""
         response = self.client.get(self.browse_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'browse.html', 'base.html')
 
-    # def test_browse_GET_not_logged_in(self):
-    #     self.client.logout()
-    #     response = self.client.get(self.browse_url)
-    #     self.assertEquals(response.status_code, 302)
+    def test_browse_GET_not_logged_in(self):
+        """Test to check browse page is accessible when not logged in"""
+        self.client.logout()
+        response = self.client.get(self.browse_url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_add_post_POST(self):
+        """Test to check add post feature writes to database"""
+        response = self.client.post(self.add_post_url, {
+            'title': 'Test title 2',
+            'slug': 'test-title-2',
+            'author': self.user.id,
+            'location': 'Test location 2',
+            'opening_time': 2,
+            'closing_time': 2,
+            'website': 'www.testurl2.com',
+            'content': 'test content for test blog post 2',
+            'featured_image': 'test_img_2.jpeg',
+            'status': 1,
+            'category': self.category.id
+        })
+
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(Post.objects.count(), 2)
+        self.assertEquals(Post.objects.last().title, 'Test title')
+
+    def test_post_no_data_POST(self):
+        """Test to check add post feature does not write to database with no data"""
+        response = self.client.post(self.add_post_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(Post.objects.count(), 1)
+
+    def test_post_update_POST(self):
+        """Test to check update post feature updates database"""
+        response = self.client.post(self.update_post_url, {
+            'title': 'Test title 3',
+            'slug': 'test-title-2',
+            'author': self.user.id,
+            'location': 'Test location 3',
+            'opening_time': 2,
+            'closing_time': 2,
+            'website': 'www.testurl3.com',
+            'content': 'test content for test blog post 2',
+            'featured_image': 'test_img_2.jpeg',
+            'status': 1,
+            'category': self.category.id
+        })
+
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(Post.objects.first().title, 'Test title 3')
+        self.assertEquals(Post.objects.count(), 1)
+
+    def test_delete_POST_DELETE(self):
+        """Test to check delete post feature deletes data from database"""
+        response = self.client.delete(self.delete_post_url)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(Post.objects.count(), 0)
+
+      
+
+
+
