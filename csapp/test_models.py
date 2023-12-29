0fad177545cd1b921f5870815d7ecbe8db2b2732
MattuW4 <>
2023-12-29 20:44:31 +0000

    Add default value tests to the test_models.py file

--- csapp/test_models.py
+++ csapp/test_models.py
@@ -6,31 +6,33 @@ from .models import Post, Category, Comment
 class TestModels(TestCase, Client):
 
     def setUp(self):
-        """Set up before each text"""
-        self.user = User.objects.create_user(username='testuser', password='password')
+        """Set up before each test"""
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='password')        
+        self.client.login(username='testuser', password='password')
         self.category = Category.objects.create(name='Test Category')
-
-    def test_model_Post(self):
-        post = Post.objects.create(
+        self.user.save()
+        
+        self.post = Post.objects.create(
             title='Test title',
             slug='test-title',
             author=self.user,
             location='Test location',
-            opening_time=1,
-            closing_time=1,
+            opening_time=0,
+            closing_time=0,
             website='www.testurl.com',
             content='test content for test blog post',
             featured_image='test_img.jpeg',
-            status=1,
+            status=0,
             category=self.category
         )
         
-        self.assertEquals(str(post), 'Test title')
-        self.assertTrue(isinstance(post, Post))
+        # self.assertEquals(str(post), 'Test title')
+        # self.assertTrue(isinstance(post, Post))
     
-    # def test_post_defaults(self):
-    #     """Test default values"""
-        # self.assertTrue(self.post.opening_time == 0)
-        # self.assertTrue(self.post.closing_time == 0)
-        # self.assertTrue(self.post.status == 0)
+    def test_post_defaults(self):
+        """Test default values"""
+        self.assertTrue(self.post.opening_time == 0)
+        self.assertTrue(self.post.closing_time == 0)
+        self.assertTrue(self.post.status == 0)
         # self.assertTrue(self.comment.approved)
